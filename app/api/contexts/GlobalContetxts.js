e50c119b086f2b03d34bb6e0caf37585d6ade202
mdotdotdot <116056922+mdotdotdot@users.noreply.github.com>
2023-05-11 15:01:55 +0500

    Delete app directory

--- app/api/contexts/GlobalContetxts.js
+++
@@ -1,6 +0,0 @@
-"use client";
-import { createContext, useState } from "react";
-
-const GlobalContext = createContext();
-
-export default GlobalContext;
--- app/register/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import RegisterPage from "../../components/registerpage";
-export default function Reg() {
-  return (
-    <div className="h-screen w-screen flex flex-col justify-center">
-      <RegisterPage />
-    </div>
-  );
-}
